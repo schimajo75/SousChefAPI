47f33050f087e06cc95fbd63e6643d09dad16e6d
github username <github email address>
2020-08-11 15:27:13 -0400

    fixed seed data

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
   has_many :recipe_lists
   has_many :recipes, through: :recipe_lists
+  has_many :notes, through: :recipe_lists
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,4 +2,5 @@ class UserSerializer < ActiveModel::Serializer
   attributes :id, :name, :email
   has_many :recipe_lists
   has_many :recipes, through: :recipe_lists
+  has_many :notes, through: :recipe_lists
 end
